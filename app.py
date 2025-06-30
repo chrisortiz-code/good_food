894c8ba89fe6d3803bdbc4caa68e853be91b488e
chriso124 <chriso888124@gmail.com>
2025-06-30 11:50:48 -0400

    image work need stats

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, render_template_string, request, redirect
 import sqlite3
 import datetime
 
@@ -9,7 +9,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 DB_PATH = "Databases/good_food.db"
 
-UPLOAD_FOLDER = "images"
+UPLOAD_FOLDER = "static/images"
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -36,43 +36,47 @@ def checkout():
         return redirect("/")
     summary = ", ".join(f"{k} ({v})" for k, v in filtered.items())
     total = 0
-    for name, qty in quantities.items():
-        for pname, price, _ in products:
-            if pname==name:
-                total+=price * qty
-                break
+    receipt = []
+    for name, price,_ in products:
+        qty = filtered.get(name)
+        if qty:
+            line_total=  qty*price
+            receipt.append((name,qty,line_total))
+            total+=line_total
+        
     timestamp = datetime.datetime.today().strftime("%Y-%m-%d %H:%M")
+
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute("INSERT INTO orders (date, products, total) VALUES (?, ?, ?)", (timestamp, summary, total))
     conn.commit()
     conn.close()
-    return render_template("/gracias.html")
+    return render_template("/gracias.html", receipt = receipt, total = total)
 
 @app.route("/products")
 def product_manager():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute("SELECT name, price FROM products")
+    cursor.execute("SELECT name, price, image FROM products")
     products = cursor.fetchall()
     conn.close()
     return render_template("products.html", products=products)
-
-@app.route("/products/update", methods=["POST"])
-def update_product():
+from pathlib import Path
+@app.route("/products/update/<name>", methods=["POST"])
+def update_product(name):
     name = request.form["name"]
     price = float(request.form["price"])
     new_name = request.form["new_name"]
-    
     image = request.files.get("image")
 
-    if image and image.filename:
+    if image and image.filename: 
+
         filename = secure_filename(image.filename)
-        image_path = os.path.join(UPLOAD_FOLDER,filename)
-        image.save(image_path)
+        save_path = Path(UPLOAD_FOLDER)/ filename
+        image.save(str(save_path))
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute("UPDATE products SET name = ?, price = ?, image = ? WHERE name = ?", (new_name, price, image, name))
+    cursor.execute("UPDATE products SET name = ?, price = ?, image = ? WHERE name = ?", (new_name, price, str(save_path), name))
     conn.commit()
     conn.close()
     return redirect("/products")
@@ -113,6 +117,14 @@ def chart():
     conn.close()
     return render_template("chart.html", orders = data)
 
+# @app.route("/test")
+# def test():
+#     conn = sqlite3.connect(DB_PATH)
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT name, price, image FROM products")
+#     products = cursor.fetchall()
+#     conn.close()
+#     return render_template("test.html",products = products)
 
 
 if __name__ == '__main__':
